411de6234d6fbd4d4991ea55eec9477fceaf84e1
ValentinDelfin <valentindelfin4@gmail.com>
2023-10-28 04:11:08 -0300

    Merge origin/master
    Conflicts:
    	tallerPOO/pom.xml
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/AdmisionDeEmergencia.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/AsignacionBox.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Box.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/FuncionarioAdministrativo.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Medico.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Paciente.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Persona.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/ResultadoEstudios.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/TallerPOO.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Triage.java

---
+++ tallerPOO/src/main/java/interfas/PantallaLoginnnnn.java
@@ -0,0 +1,266 @@
+
+package interfas;
+import persistencia.ValidarSesion;
+import logica.ControlLogin;
+import java.awt.Window;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+import java.util.Formatter;
+import java.util.Properties;
+import logica.ControlLogin;
+public class PantallaLoginnnnn extends javax.swing.JFrame {
+    String barra = File.separator;
+    // Ruta relativa a la carpeta "TallerPoovvvv/Registro" dentro del proyecto
+    String Ubicacion = System.getProperty("user.dir") + barra + "BaseDatos"+barra;
+
+    public PantallaLoginnnnn() {
+        initComponents();
+    }
+    
+    private void registrar() {
+        String nombreUsuario = txtUsuario.getText();
+        String nombreArchivo = nombreUsuario + ".txt"; // Construye el nombre del archivo
+
+        // Crea la carpeta si no existe
+        File crearubi = new File(Ubicacion);
+        crearubi.mkdirs();
+
+        File creararchivo = new File(Ubicacion + barra + nombreArchivo); // Crea el archivo
+
+       if(nombreUsuario.isEmpty() || txtContrasenia.getText().equals("")){
+            JOptionPane.showMessageDialog(rootPane,"Complete los campos");
+        } else {
+            try {
+                if (creararchivo.exists()) {
+                    JOptionPane.showMessageDialog(rootPane, "El usuario ya está registrado");
+                } else {
+                    // Escribe en el archivo
+                    Formatter crearformater = new Formatter(creararchivo);
+                    crearformater.format("Usuario: %s%nContraseña: %s", nombreUsuario, txtContrasenia.getText());
+
+                    crearformater.close();
+                    JOptionPane.showMessageDialog(rootPane, "Registro exitoso");
+                }
+                
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(rootPane, "Registro incorrecto");
+            }
+        }
+    }
+    /*private void sesion(){
+        //obtengo el nombre del usuario dentro del archivo
+        File direccion = new File(Ubicacion + txtUsuario.getText()+ ".txt" );
+        
+        if(txtUsuario.getText().equals("") || txtContrasenia.getText().equals("")){
+            JOptionPane.showMessageDialog(rootPane,"Complete los campos");
+        }else{
+            if(direccion.exists()){
+                try{
+                    FileInputStream aValidar = new FileInputStream(direccion);
+                    Properties validar = new Properties();
+                    validar.load(aValidar);
+                    String contraseña = validar.getProperty("Contraseña");
+                    if(contraseña.equals(txtContrasenia.getText())){
+                   dispose();
+                   /* OpcionesDoc cambio = new OpcionesDoc();
+                    cambio.setVisible(true);
+                    cambio.setLocationRelativeTo(null);*/
+                       
+                   /*ControlLogin control = new ControlLogin();
+                   control.rol(txtUsuario.getText());
+                    }else{
+                    JOptionPane.showMessageDialog(rootPane,"Usuario y/o contraseña incorrecto");
+                    }
+                }catch(Exception e){
+                }
+            }else{
+                JOptionPane.showMessageDialog(rootPane, "Usuario y/o contraseña incorrecto");
+                
+            }
+                
+        }*/
+ 
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        sesion1 = new logica.Sesion();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
+        txtUsuario = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        btnLogin = new javax.swing.JButton();
+        txtContrasenia = new javax.swing.JPasswordField();
+        btnInicioSesion = new javax.swing.JButton();
+
+        jButton1.setText("jButton1");
+
+        jLabel1.setText("jLabel1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel4.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jLabel4.setText("Login");
+
+        jLabel5.setText("Usuario");
+
+        txtUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtUsuarioActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Contraseña");
+
+        btnLogin.setText("Registrar");
+        btnLogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoginActionPerformed(evt);
+            }
+        });
+
+        txtContrasenia.setText("jPasswordField1");
+
+        btnInicioSesion.setText("Iniciar Sesion");
+        btnInicioSesion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnInicioSesionActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtUsuario)
+                    .addComponent(txtContrasenia, javax.swing.GroupLayout.DEFAULT_SIZE, 218, Short.MAX_VALUE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel3))
+                        .addGap(0, 152, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnInicioSesion)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(9, 9, 9)
+                        .addComponent(btnLogin)))
+                .addGap(71, 71, 71))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(txtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtContrasenia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnInicioSesion)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                .addComponent(btnLogin)
+                .addGap(23, 23, 23))
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(80, 80, 80)
+                .addComponent(jLabel4)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void txtUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUsuarioActionPerformed
+                 
+    }//GEN-LAST:event_txtUsuarioActionPerformed
+
+    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
+        registrar();
+       
+    }//GEN-LAST:event_btnLoginActionPerformed
+
+    private void btnInicioSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInicioSesionActionPerformed
+       
+     ValidarSesion control = new ValidarSesion();
+     control.sesion(txtUsuario.getText(), txtContrasenia.getText());
+     dispose();
+    
+      
+    }//GEN-LAST:event_btnInicioSesionActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+   
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnInicioSesion;
+    private javax.swing.JButton btnLogin;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private logica.Sesion sesion1;
+    private javax.swing.JPasswordField txtContrasenia;
+    private javax.swing.JTextField txtUsuario;
+    // End of variables declaration//GEN-END:variables
+
+    private void JPassworldField(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
