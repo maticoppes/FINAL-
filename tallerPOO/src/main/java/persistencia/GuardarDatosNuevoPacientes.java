329171babebdad86164cf6d13da403635cc1dbfe
DESKTOP-TS2NHON\Matias <matiasareguaticoppes@gmail.com>
2023-10-28 20:38:21 -0300

    dsdasd

--- tallerPOO/src/main/java/persistencia/GuardarDatosNuevoPacientes.java
+++ tallerPOO/src/main/java/persistencia/GuardarDatosNuevoPacientes.java
@@ -8,10 +8,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
+/**
+ * Esta clase se encarga de guardar los datos de nuevos pacientes en un archivo.
+ * Puede recibir una ubicación y los datos a guardar, y los agrega al archivo.
+ * 
+ * @see interfas.IngresoInfoFuncionario
+ * @see interfas.RegistrarNuevoPaciente
+ */
 
 public class GuardarDatosNuevoPacientes {
-
+    /**
+     * Carga los datos proporcionados en un archivo especificado por la ubicación.
+     * 
+     * @param Ubicacion La ubicación del archivo donde se guardarán los datos.
+     * @param datos     Los datos a guardar en el archivo.
+     */
     public void Carga(String Ubicacion, String datos) {
         System.out.println(Ubicacion);
         File archivo = new File(Ubicacion);
