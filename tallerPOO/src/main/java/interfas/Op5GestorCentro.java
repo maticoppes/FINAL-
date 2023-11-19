ae3618165963d613a9e4df1fc9c98e841b15fd21
ValentinDelfin <valentindelfin4@gmail.com>
2023-11-19 17:06:39 -0300

    Merge origin/master
    Conflicts:
    	tallerPOO/pom.xml
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/AdmisionDeEmergencia.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/AsignacionBox.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Box.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/FuncionarioAdministrativo.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Medico.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Paciente.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Persona.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/ResultadoEstudios.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/TallerPOO.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Triage.java

--- tallerPOO/src/main/java/interfas/Op5GestorCentro.java
+++ tallerPOO/src/main/java/interfas/Op5GestorCentro.java
@@ -227,6 +227,8 @@ public class Op5GestorCentro extends javax.swing.JFrame {
     private void buscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buscarActionPerformed
         String fechaUno = fecha1.getText();
         String fechaDos = fecha2.getText();
+        DefaultTableModel model= (DefaultTableModel) tablaTriage.getModel();
+        model.setRowCount(0);
         if (validarFormatoFecha(fechaUno) && validarFormatoFecha(fechaDos)) {
             DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy/MM/dd");
             LocalDate primeraFecha = LocalDate.parse(fechaUno, formatter1);
