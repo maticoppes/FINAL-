280b10670d110b304f7ec688aea8c93614b7e318
ValentinDelfin <valentindelfin4@gmail.com>
2023-11-20 18:21:56 -0300

    Merge origin/master
    Conflicts:
    	tallerPOO/pom.xml
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/AdmisionDeEmergencia.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/AsignacionBox.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Box.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/FuncionarioAdministrativo.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Medico.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Paciente.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Persona.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/ResultadoEstudios.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/TallerPOO.java
    	tallerPOO/src/main/java/com/mycompany/tallerpoo/Triage.java

--- tallerPOO/src/main/java/interfas/PacientesConTriage.java
+++ tallerPOO/src/main/java/interfas/PacientesConTriage.java
@@ -95,6 +95,7 @@ public class PacientesConTriage extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         Tabla = new javax.swing.JTable();
         btnActualizar = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -145,6 +146,13 @@ public class PacientesConTriage extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -155,7 +163,9 @@ public class PacientesConTriage extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 474, Short.MAX_VALUE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnActualizar)
                 .addGap(14, 14, 14))
         );
@@ -166,7 +176,9 @@ public class PacientesConTriage extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btnActualizar)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnActualizar)
+                    .addComponent(jButton1))
                 .addContainerGap(14, Short.MAX_VALUE))
         );
 
@@ -177,6 +189,13 @@ public class PacientesConTriage extends javax.swing.JFrame {
         llamar();
     }//GEN-LAST:event_btnActualizarActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        InterfazDoc objeto = new InterfazDoc();
+        objeto.setVisible(true);
+        objeto.setLocationRelativeTo(null);
+        dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -184,6 +203,7 @@ public class PacientesConTriage extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable Tabla;
     private javax.swing.JButton btnActualizar;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
